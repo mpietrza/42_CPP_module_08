7d10dc0823068c903cf2e40b8a21340f7a7ff0b0
mpietrza <mpietrza@student.42barcelona.com>
2025-08-04 17:03:08 +0200

    ex02.1

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpietrza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 16:49:53 by mpietrza          #+#    #+#             */
+/*   Updated: 2025/08/04 17:02:50 by mpietrza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_HPP
+# define MUTANT_STACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+
+
+#endif
